34386bb84385bc5396fc3f59019ee40160e43a47
Anna Holmgren <anna.holmgren@wigetmedia.com>
2015-12-07 11:41:54 +0100

    Dec05 code + tests

--- src/dec05.py
+++ src/dec05.py
@@ -1,21 +1,56 @@
 import fileinput
+import re
 
 __author__ = 'Anna'
 
+BAD = re.compile('ab|cd|pq|xy')
+VOWELS = re.compile('[aeiou].*[aeiou].*[aeiou]')
+DOUBLE = re.compile('(.)\\1')
+DOUBLE_PAIR = re.compile('(..).*\\1')
+REPEATING_LETTER = re.compile('(.).\\1')
 
-VOWELS = 'aeiou'
-BAD = ['ab', 'cd', 'pq', 'xy']
+
+def bad_substring(word):
+    return re.search(BAD, word)
 
 
-def naughty_or_nice(word):
-    if any([word.contains(bad) for bad in BAD]):
+def three_wovels(word):
+    return re.search(VOWELS, word)
+
+
+def double_letter(word):
+    return re.search(DOUBLE, word)
+
+
+def naughty_or_nice_1(word):
+    if bad_substring(word):
         return False
+    if not three_wovels(word):
+        return False
+    if not double_letter(word):
+        return False
+    return True
+
+
+def double_pair(word):
+    return re.search(DOUBLE_PAIR, word)
+
+
+def repeating_letter(word):
+    return re.search(REPEATING_LETTER, word)
+
+
+def naughty_or_nice_2(word):
+    return double_pair(word) and repeating_letter(word)
 
 
 if __name__ == '__main__':
-    nice = 0
+    nice_1 = 0
+    nice_2 = 0
     for line in fileinput.input('../data/input.5.txt'):
-        if naughty_or_nice(line):
-            nice += 1
+        if naughty_or_nice_1(line):
+            nice_1 += 1
+        if naughty_or_nice_2(line):
+            nice_2 += 1
 
-    print('Nice words: {nice}'.format(nice=nice))
+    print('Nice words: 1: {nice}, 2: {nice2}'.format(nice=nice_1, nice2=nice_2))
---
+++ test/test_dec05.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import unittest
+from src import dec05
+
+__author__ = 'anna'
+
+
+class Dec04Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testNaughtyOrNiceExample1(self):
+        self.assertTrue(dec05.naughty_or_nice_1('ugknbfddgicrmopn'))
+
+    def testNaughtyOrNiceExample2(self):
+        self.assertTrue(dec05.naughty_or_nice_1('aaa'))
+
+    def testNaughtyOrNiceExample3(self):
+        self.assertFalse(dec05.naughty_or_nice_1('jchzalrnumimnmhp'))
+
+    def testNaughtyOrNiceExample4(self):
+        self.assertFalse(dec05.naughty_or_nice_1('haegwjzuvuyypxyu'))
+
+    def testNaughtyOrNiceExample5(self):
+        self.assertFalse(dec05.naughty_or_nice_1('dvszwmarrgswjxmb'))
+
+    def testNaughtyOrNiceExample6(self):
+        self.assertTrue(dec05.naughty_or_nice_2('qjhvhtzxzqqjkmpb'))
+
+    def testNaughtyOrNiceExample7(self):
+        self.assertTrue(dec05.naughty_or_nice_2('xxyxx'))
+
+    def testNaughtyOrNiceExample8(self):
+        self.assertFalse(dec05.naughty_or_nice_2('uurcxstgmygtbstg'))
+
+    def testNaughtyOrNiceExample9(self):
+        self.assertFalse(dec05.naughty_or_nice_2('ieodomkazucvgmuy'))
