02fa2e8c25766fb123b8b525565eff4bd81c6e8e
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-01-05 16:48:00 +0100

    Dec19 part 2. Giving up and doing the ugly count version. Let's see if I try actually bruteforcing my way to the solution later on.

--- python/src/dec19.py
+++ python/src/dec19.py
@@ -5,21 +5,15 @@ __author__ = 'Anna'
 
 class Rule(object):
 
-    RE_RULE = re.compile(r'(\w+) => (\w+)')
+    RE_RULE = re.compile(r'(\w+) => (.*)\n?')
 
     def __init__(self, rule):
         result = self.RE_RULE.match(rule)
         if result:
             self.source = result.group(1)
             self.target = result.group(2)
-
-    def transform_start(self, text):
-        if text.startswith(self.source):
-            return self.target, text[len(self.source):]
-        elif len(text) > 1:
-            return text[0], text[1:]
         else:
-            return text[0], None
+            raise ValueError(rule + " didn't match")
 
     def transform_any(self, text):
         found = text.find(self.source)
@@ -34,19 +28,44 @@ class Rule(object):
         return "{0} => {1}".format(self.source, self.target)
 
 
-def replace_molecules(rules_as_text, start_string):
-    rules = [Rule(rule) for rule in rules_as_text]
-    replacements = list()
-    for r in rules:
-        replacements.extend(r.transform_any(start_string))
-    return list(set(replacements))
+def sanitize(text):
+    text = text.replace('Ar', ')')
+    text = text.replace('Rn', '(')
+    text = text.replace('Y', ',')
+    return text
+
+
+class FusionFission(object):
+    RE_TOKEN = re.compile(r'((?:[A-Z][a-z]*)|(?:[\(\),]))')
+
+    def __init__(self, rules_as_text, start_string):
+        self.rules = [Rule(sanitize(rule)) for rule in rules_as_text]
+        self.text = sanitize(start_string)
+        self.text_tokens = self.RE_TOKEN.findall(self.text)
+
+    def one_step(self):
+        replacements = list()
+        for r in self.rules:
+            replacements.extend(r.transform_any(self.text))
+        return list(set(replacements))
+
+    def ugly_count(self):
+        # https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju
+        parentheses = self.text.count('(') + self.text.count(')')
+        comma = self.text.count(',')
+        length = len(self.text_tokens)
+        return length - parentheses - 2*comma - 1
 
 
 def main():
     with open('../../data/input.19.txt') as fh:
         lines = fh.readlines()
-        replacements = replace_molecules(lines[:-2], lines[-1])
+        rules = lines[:-2]
+        text = lines[-1].strip()
+        r = FusionFission(rules, text)
+        replacements = r.one_step()
         print("We can create {0} replacements".format(len(replacements)))
+        print("It takes {0} steps to reach the medicine".format(r.ugly_count()))
 
 if __name__ == '__main__':
     main()
--- python/test/test_dec19.py
+++ python/test/test_dec19.py
@@ -11,22 +11,25 @@ class Dec19Tests(unittest.TestCase):
                  "H => OH",
                  "O => HH"]
         start_string = "HOH"
-        self.assertEqual(4, len(dec19.replace_molecules(rules, start_string)))
+        r = dec19.FusionFission(rules, start_string)
+        self.assertEqual(4, len(r.one_step()))
 
     def test_count_replace_example2(self):
         rules = ["H => HO",
                  "H => OH",
                  "O => HH"]
         start_string = "HOHOHO"
-        self.assertEqual(7, len(dec19.replace_molecules(rules, start_string)))
+        r = dec19.FusionFission(rules, start_string)
+        self.assertEqual(7, len(r.one_step()))
 
     @unittest.skip
     def test_count_replace_example3(self):
         rules = ["H => OO"]
         start_string = "H2O"
-        rep = dec19.replace_molecules(rules, start_string)
-        self.assertEqual(1, len(rep))
+        r = dec19.FusionFission(rules, start_string)
+        rep = r.one_step()
         self.assertEqual("OO2O", rep[0])
+        self.assertEqual(1, len(rep))
 
     def test_transform_example1(self):
         rule = dec19.Rule('H => OO')
@@ -39,7 +42,6 @@ class Dec19Tests(unittest.TestCase):
         text = "H2H"
         expected = ["OO2H", "H2OO"]
         self.assertListEqual(sorted(expected), sorted(rule.transform_any(text)))
-
 
 if __name__ == '__main__':
     unittest.main()
