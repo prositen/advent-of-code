74cbe73a2ad29aa7f9240aac9a85ed8799f9e66b
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-12-01 18:27:52 +0100

    2015-12-24

---
+++ python/src/y2015/dec24.py
@@ -0,0 +1,51 @@
+from functools import reduce
+from itertools import permutations
+
+
+def quantum_entanglement(packages):
+    return reduce(lambda x, y: x*y, packages)
+
+
+def smallest_group(packages, number_of_groups):
+    weight = sum(packages) / number_of_groups
+    package_count = len(packages)
+
+    sorted_min = sorted(packages)
+    sorted_max = packages[::-1]
+    cumulative_min = [sum(sorted_min[:x]) for x in range(1, package_count)]
+    cumulative_max = [sum(sorted_max[:x]) for x in range(1, package_count)]
+    min_items_needed = 0
+    max_items_needed = 0
+
+    for index, num in enumerate(cumulative_max):
+        if num >= weight:
+            min_items_needed = index
+            break
+    for index, num in enumerate(cumulative_min):
+        if num >= weight:
+            max_items_needed = index
+            break
+
+    print("Smallest amount of items needed", min_items_needed)
+    print("Largest amount of items needed", max_items_needed)
+    perms = set()
+    for n in range(min_items_needed, max_items_needed):
+        print("Attempting with", n, "items")
+        perms = set(filter(lambda x: sum(x) == weight, permutations(packages, n)))
+        if len(perms):
+            break
+
+    groups = sorted(perms, key=lambda x: (len(x), quantum_entanglement(x)))
+    return groups[0]
+
+
+if __name__ == '__main__':
+    with open('../../../data/2015/input.24.txt', 'r') as fh:
+        packages = [int(p) for p in fh.readlines()]
+        print("Part 1. This takes a couple of minutes")
+        smallest = smallest_group(packages, 3)
+        print(smallest, quantum_entanglement(smallest))
+        # This is quick!
+        print("Part 2")
+        smallest = smallest_group(packages, 4)
+        print(smallest, quantum_entanglement(smallest))
---
+++ python/test/y2015/test_dec24.py
@@ -0,0 +1,14 @@
+from python.src.y2015 import dec24
+import unittest
+
+
+class Dec23Tests(unittest.TestCase):
+
+    def test_quantum_entanglement(self):
+        self.assertEquals(99, dec24.quantum_entanglement([11, 9]))
+
+    def test_smallest_group(self):
+        self.assertSetEqual({9, 11}, set(dec24.smallest_group([1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 3)))
+
+    def test_smallest_group_part_2(self):
+        self.assertSetEqual({11, 4}, set(dec24.smallest_group([1, 2, 3, 4, 5, 7, 8, 9, 10, 11], 4)))
