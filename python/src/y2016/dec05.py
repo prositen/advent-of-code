b41d9b52cadf786272bff00d13b4fd4037290930
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-12-05 08:48:02 +0100

    2016-12-05

---
+++ python/src/y2016/dec05.py
@@ -0,0 +1,44 @@
+__author__ = 'Anna'
+from hashlib import md5
+
+SECRET = 'uqwqemis'
+
+
+def hash_password(secret, password_letters=8, zeroes=5):
+    number = 0
+    hashcode = ''
+    z = '0' * zeroes
+    password = ''
+    for n in range(password_letters):
+        while not hashcode.startswith(z):
+            number += 1
+            value = ("%s%d" % (secret,  number)).encode('utf-8')
+            hashcode = md5(value).hexdigest()
+        password += hashcode[zeroes]
+        hashcode = ''
+
+    return password
+
+
+def movie_password(secret, password_letters=8, zeroes=5):
+    number = 0
+    hashcode = ''
+    z = '0' * zeroes
+    password = ['_'] * password_letters
+    while '_' in password:
+        while not hashcode.startswith(z):
+            number += 1
+            value = ("%s%d" % (secret,  number)).encode('utf-8')
+            hashcode = md5(value).hexdigest()
+        position = int(hashcode[zeroes], 16)
+        character = hashcode[zeroes+1]
+        if position < password_letters and password[position] == '_':
+            password[position] = character
+            print(''.join(password))
+        hashcode = ''
+
+    return ''.join(password)
+
+if __name__ == '__main__':
+    print("Password:", hash_password(SECRET))
+    print("Movie style password", movie_password(SECRET))
---
+++ python/test/y2016/test_dec05.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import unittest
+
+from python.src.y2016 import dec05
+
+__author__ = 'anna'
+
+
+class Dec04Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testMiningHashesExample1(self):
+        self.assertEqual('1', dec05.hash_password('abc', 1))
+
+    def testMiningHashesExample2(self):
+        self.assertEqual('18', dec05.hash_password('abc', 2))
+
+    def testMoviePassword(self):
+        self.assertEquals('05ace8e3', dec05.movie_password('abc'))
+
+if __name__ == '__main__':
+    unittest.main()
