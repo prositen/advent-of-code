9ea67e98f281e1c3e70c6e482f0b616b3e41d84e
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-12-07 11:48:12 +0100

    2016-12-07

---
+++ python/src/y2016/dec07.py
@@ -0,0 +1,54 @@
+import re
+
+re_HYPERNET = re.compile(r"(\[\w+\])")
+re_ABBA = re.compile(r"(\w)((?!\1)\w)\2\1")
+re_ABA = re.compile(r"(?=((\w)((?!\2)\w)\2))")
+
+
+def split_ip_hypernets(ip):
+    hyper, not_hyper = [], []
+    start = 0
+    for match in re_HYPERNET.finditer(ip):
+        match_start = match.start()
+        match_end = match.end()
+        if match_start > start:
+            not_hyper.append(ip[start:match_start])
+        start = match_end
+        hyper.append(match.group(0))
+    not_hyper.append(ip[start:])
+    return hyper, not_hyper
+
+
+def ipv7_supports_tls(ip):
+    hyper, non_hyper = split_ip_hypernets(ip)
+    if any(re_ABBA.search(x) for x in hyper):
+        return False
+    else:
+        return any(re_ABBA.search(x) for x in non_hyper)
+
+
+def ipv7_supports_ssl(ip):
+    hyper, non_hyper = split_ip_hypernets(ip)
+    hyper = "".join(hyper)
+    non_hyper = "-".join(non_hyper)
+
+    for match in re_ABA.finditer(non_hyper):
+        ABA = match.group(1)
+        BAB = ABA[1] + ABA[0] + ABA[1]
+        if BAB in hyper:
+            return True
+    return False
+
+
+def count_ips_with_tls(ips):
+    return len(list(filter(ipv7_supports_tls, ips)))
+
+
+def count_ips_with_ssl(ips):
+    return len(list(filter(ipv7_supports_ssl, ips)))
+
+if __name__ == '__main__':
+    with open('../../../data/2016/input.7.txt', 'r') as fh:
+        lines = fh.readlines()
+        print("Number of IPs that support TLS", count_ips_with_tls(lines))
+        print("Number of IPs that support SSL", count_ips_with_ssl(lines))
---
+++ python/test/y2016/test_dec07.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import unittest
+
+from python.src.y2016 import dec07
+
+__author__ = 'anna'
+
+
+class Dec07Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_supports_tls_1(self):
+        self.assertTrue(dec07.ipv7_supports_tls('abba[mnop]qrst'))
+
+    def test_supports_tls_2(self):
+        self.assertFalse(dec07.ipv7_supports_tls('abcd[bddb]xyyx'))
+
+    def test_supports_tls_3(self):
+        self.assertFalse(dec07.ipv7_supports_tls('aaaa[qwer]tyui'))
+
+    def test_supports_tls_4(self):
+        self.assertTrue(dec07.ipv7_supports_tls('ioxxoj[asdfgh]zxcvbn'))
+
+    def test_supports_ssl_1(self):
+        self.assertTrue(dec07.ipv7_supports_ssl('aba[bab]xyz'))
+
+    def test_supports_ssl_2(self):
+        self.assertFalse(dec07.ipv7_supports_ssl('xyx[xyx]xyx'))
+
+    def test_supports_ssl_3(self):
+        self.assertTrue(dec07.ipv7_supports_ssl('aaa[kek]eke'))
+
+    def test_supports_ssl_4(self):
+        self.assertTrue(dec07.ipv7_supports_ssl('zazbz[bzb]cdb'))
+
+    if __name__ == '__main__':
+        unittest.main()
