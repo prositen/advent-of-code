f040cd4ad91f09a563d6cdfa1cc8a9da8ecfee58
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-12-08 11:10:33 +0100

    2016-12-08

---
+++ python/test/y2016/test_dec08.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import unittest
+
+from python.src.y2016 import dec08
+
+__author__ = 'anna'
+
+
+class Dec08Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_simple_sequence(self):
+        grid = dec08.Screen(7, 3)
+        grid.rect(3, 2)
+        self.assertListEqual(['###....',
+                              '###....',
+                              '.......'],
+                             grid.get_rows())
+        grid.rotate_column(1, 1)
+        self.assertListEqual(['#.#....',
+                              '###....',
+                              '.#.....'],
+                             grid.get_rows())
+        grid.rotate_row(0, 4)
+        self.assertListEqual(['....#.#',
+                              '###....',
+                              '.#.....'],
+                             grid.get_rows())
+        grid.rotate_column(1, 1)
+        self.assertListEqual(['.#..#.#',
+                              '#.#....',
+                              '.#.....'],
+                             grid.get_rows())
+
+        self.assertEquals(6, grid.count_lit())
+
+    if __name__ == '__main__':
+        unittest.main()
