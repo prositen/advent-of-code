b840ad85a472c14174b826ccef45139d131a9a74
Anna Holmgren <anna.holmgren@advanze.se>
2017-12-10 16:37:35 +0100

    2017-12-10 Added distinct methods to create a KnotHash in different ways

--- python/test/y2017/test_dec10.py
+++ python/test/y2017/test_dec10.py
@@ -4,7 +4,7 @@ from python.src.y2017 import dec10
 
 class TestKnotHash(unittest.TestCase):
     def test_check_first_two(self):
-        kh = dec10.KnotHash("3,4,1,5", input_is_bytes=False, list_length=5)
+        kh = dec10.KnotHash.from_string_input("3,4,1,5", 5)
         kh.round()
         self.assertEqual(12, kh.check_first_two())
 
@@ -16,6 +16,6 @@ class TestKnotHash(unittest.TestCase):
             ("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")
         ]
         for lengths, expected_result in test_cases:
-            kh = dec10.KnotHash(lengths)
+            kh = dec10.KnotHash.from_bytes_input(lengths)
             kh.run()
             self.assertEqual(expected_result, kh.checksum())
