8d3ed4976fe9e73ccb4ad8808db10124ed5ec086
Anna Holmgren <anna.holmgren@wigetmedia.com>
2017-12-22 07:32:39 +0100

    2017-12-22

--- python/test/y2015/test_dec06.py
+++ python/test/y2015/test_dec06.py
@@ -13,7 +13,7 @@ class Dec06Tests(unittest.TestCase):
         self.assertEquals(1000000, dec06.lightning(instructions))
 
     def testLightingExample2(self):
-        instructions = ["toggle 0,0 through 999,0"]
+        instructions = ["work 0,0 through 999,0"]
         self.assertEquals(1000, dec06.lightning(instructions))
 
     def testLightingExample3(self):
@@ -23,8 +23,8 @@ class Dec06Tests(unittest.TestCase):
 
     def testLightning_debug1(self):
         instructions = ["turn on 0,0 through 9,9",
-                        "toggle 5,5 through 5,5",
-                        "toggle 1,1 through 1,1"]
+                        "work 5,5 through 5,5",
+                        "work 1,1 through 1,1"]
         self.assertEquals(98, dec06.lightning(instructions))
 
     def testBrightnessExample1(self):
@@ -32,7 +32,7 @@ class Dec06Tests(unittest.TestCase):
         self.assertEquals(1, dec06.brightness(instructions))
 
     def testBrightnessExample2(self):
-        instructions = ["toggle 0,0 through 999,999"]
+        instructions = ["work 0,0 through 999,999"]
         self.assertEquals(2000000, dec06.brightness(instructions))
 
 if __name__ == '__main__':
