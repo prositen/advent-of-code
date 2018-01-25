dce764859c85322a52bb77bd1e8950ff7e2df4d4
Anna Holmgren <prositen@gmail.com>
2018-01-25 16:49:00 +0100

    Add tests for bash solutions (#71)

--- bash/2017/dec01.bash
+++ bash/2017/dec01.bash
@@ -21,7 +21,34 @@ function captcha() {
 	echo "$sum"
 }
 
+function dec01_test() {
 
-value=$(read_file_to_var 2017 1)
-echo "Part 1: $(captcha $value part1)"
-echo "Part 2: $(captcha $value part2)"
+    # Part 1
+    (( $(captcha 1122 part1) == 3 )) \
+    &&  (( $(captcha 1111 part1) == 4 )) \
+    && (( $(captcha 1234 part1) == 0)) \
+    && (( $(captcha 91212129 part1) == 9)) \
+    || return 1
+
+
+    # Part 2
+    (( $(captcha 1212 part2) == 6 )) \
+    && (( $(captcha 1221 part2) == 0 )) \
+    && (( $(captcha 123425 part2) == 4 )) \
+    && (( $(captcha 123123 part2) == 12)) \
+    && (( $(captcha 12131415 part2) == 4)) \
+    || return 1
+    return 0
+}
+
+function dec01_main() {
+    local value=$(read_file_to_var 2017 1)
+    echo "Part 1: $(captcha $value part1)"
+    echo "Part 2: $(captcha $value part2)"
+}
+
+if [ "x$1" == "xtest" ]; then
+    dec01_test
+else
+    dec01_main
+fi
