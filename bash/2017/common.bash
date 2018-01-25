94502be10a37f54b7be0a1425d6892c07c59294e
Anna Holmgren <anna.holmgren@advanze.se>
2018-01-25 13:31:39 +0100

    2017-12-02 in bash

--- bash/2017/common.bash
+++ bash/2017/common.bash
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+##
+# value=$(read_file_to_var year day)
+##
 function read_file_to_var() {
 	local year=$1
 	local day=$2
-	local value=$(<../../data/$year/input.$day.txt)
-	echo $value
+	local value=$(<../../data/${year}/input.${day}.txt)
+	echo ${value}
 }
+
+##
+# read_file_to_array year day value
+##
+function read_file_to_arr() {
+    # array keys: ${!array[@]}
+    local year=$1
+    local day=$2
+    readarray -t "$3" < ../../data/${year}/input.${day}.txt
+}
+
+function sort_arr() {
+    local output=$1
+    shift
+    local sort_arr_IN
+    read -r -a sort_arr_IN <<< "$@"
+
+    local sort_arr_OUT
+    IFS=$'\n' sort_arr_OUT=($(sort -n <<< "${sort_arr_IN[*]}"))
+    unset IFS
+    read -r -a "$output" <<< "${sort_arr_OUT[*]}"
+}
+
