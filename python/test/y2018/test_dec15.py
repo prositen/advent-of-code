a97525d14c010b281da6a62c3ee3ec335111d999
Anna Holmgren <anna.holmgren@advanze.se>
2018-12-17 10:06:19 +0100

    2018-12-15 Tests for part 2

--- python/test/y2018/test_dec15.py
+++ python/test/y2018/test_dec15.py
@@ -4,7 +4,7 @@ from python.src.y2018.dec15 import Dec15
 
 
 class TestDec15(unittest.TestCase):
-    def test_part_1_0(self):
+    def test_part_1_a(self):
         map_start = [
             "#######",
             "#.G...#",
@@ -16,7 +16,7 @@ class TestDec15(unittest.TestCase):
         ]
         self.assertEqual(27730, Dec15(instructions=map_start).part_1())
 
-    def test_part_1_a(self):
+    def test_part_1_b(self):
         map_start = [
             "#######",
             "#G..#E#",
@@ -28,7 +28,7 @@ class TestDec15(unittest.TestCase):
         ]
         self.assertEqual(36334, Dec15(instructions=map_start).part_1())
 
-    def test_part_1_b(self):
+    def test_part_1_c(self):
         map_start = [
             "#######",
             "#E..EG#",
@@ -40,7 +40,7 @@ class TestDec15(unittest.TestCase):
         ]
         self.assertEqual(39514, Dec15(instructions=map_start).part_1())
 
-    def test_part_1_c(self):
+    def test_part_1_d(self):
         map_start = [
             "#######",
             "#E.G#.#",
@@ -52,7 +52,7 @@ class TestDec15(unittest.TestCase):
         ]
         self.assertEqual(27755, Dec15(instructions=map_start).part_1())
 
-    def test_part_1_d(self):
+    def test_part_1_e(self):
         map_start = [
             "#######",
             "#.E...#",
@@ -64,7 +64,7 @@ class TestDec15(unittest.TestCase):
         ]
         self.assertEqual(28944, Dec15(instructions=map_start).part_1())
 
-    def test_part_1_e(self):
+    def test_part_1_f(self):
         map_start = [
             "#########",
             "#G......#",
@@ -77,3 +77,65 @@ class TestDec15(unittest.TestCase):
             "#########"
         ]
         self.assertEqual(18740, Dec15(instructions=map_start).part_1())
+
+    def test_part_2_b(self):
+        map_start = [
+            "########",
+            "# .G...#",
+            "# ...EG#",
+            "# .#.#G#",
+            "# ..G#E#",
+            "# .....#",
+            "########"
+        ]
+        self.assertEqual(4988, Dec15(instructions=map_start).part_2())
+
+    def test_part_2_c(self):
+        map_start = [
+            "#######",
+            "#E..EG#",
+            "#.#G.E#",
+            "#E.##E#",
+            "#G..#.#",
+            "#..E#.#",
+            "#######"
+        ]
+        self.assertEqual(31284, Dec15(instructions=map_start).part_2())
+
+    def test_part_2_d(self):
+        map_start = [
+            "#######",
+            "#E.G#.#",
+            "#.#G..#",
+            "#G.#.G#",
+            "#G..#.#",
+            "#...E.#",
+            "#######"
+        ]
+        self.assertEqual(3478, Dec15(instructions=map_start).part_2())
+
+    def test_part_2_e(self):
+        map_start = [
+            "#######",
+            "#.E...#",
+            "#.#..G#",
+            "#.###.#",
+            "#E#G#G#",
+            "#...#G#",
+            "######"
+        ]
+        self.assertEqual(6474, Dec15(instructions=map_start).part_2())
+
+    def test_part_2_f(self):
+        map_start = [
+            "#########",
+            "#G......#",
+            "#.E.#...#",
+            "#..##..G#",
+            "#...##..#",
+            "#...#...#",
+            "#.G...G.#",
+            "#.....G.#",
+            "#########"
+        ]
+        self.assertEqual(1140, Dec15(instructions=map_start).part_2())
