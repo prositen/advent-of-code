5f5cb6328c3b6c3c004996fc96e8c96d846efa89
Anna Holmgren <anna.holmgren@advanze.se>
2018-12-17 15:05:13 +0100

    2018-12-16

---
+++ python/test/y2018/test_dec16.py
@@ -0,0 +1,33 @@
+import unittest
+
+from python.src.y2018.dec16 import Dec16, Device
+
+
+class TestDec16(unittest.TestCase):
+
+    def test_parse(self):
+        instructions = [
+            "Before: [3, 2, 1, 1]",
+            "9 2 1 2",
+            "After:  [3, 2, 2, 1]",
+            "1 1 1 1",
+        ]
+
+        d = Dec16(instructions=instructions)
+        self.assertEqual([(
+            [3, 2, 1, 1],
+            [9, 2, 1, 2],
+            [3, 2, 2, 1]
+        )], d.samples)
+
+
+    def test_sample(self):
+        sample = (
+            [3, 2, 1, 1],
+            [9, 2, 1, 2],
+            [3, 2, 2, 1]
+        )
+        d = Device()
+        matches = d.sample(sample[0], sample[1], sample[2])
+        self.assertEqual(3, len(matches))
+        self.assertListEqual(['addi', 'mulr', 'seti'], sorted(matches))
