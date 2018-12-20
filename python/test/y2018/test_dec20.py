0da8adb17f7c9e0ff8b53a4cb0952f66032c888d
Anna Holmgren <prositen@gmail.com>
2018-12-20 16:05:34 +0100

    2018-12-20 (#93)

---
+++ python/test/y2018/test_dec20.py
@@ -0,0 +1,19 @@
+import unittest
+
+from python.src.y2018.dec20 import Dec20
+
+
+class TestDec20(unittest.TestCase):
+
+    def test_part1(self):
+        cases = [
+            (3, ['^WNE$']),
+            (10, ['^ENWWW(NEEE|SSE(EE|N))$']),
+            (18, ['^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$'])
+        ]
+
+        for expected, instruction in cases:
+            d = Dec20(instructions=instruction)
+            self.assertEqual(expected, d.part_1())
+
+
