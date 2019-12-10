341011cc5d1c1df263d01f1d157db22c3a676e54
Anna Holmgren <prositen@prositen.com>
2019-12-10 21:28:59 +0100

    Working part 1.

--- python/src/y2019/dec10.py
+++ python/src/y2019/dec10.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-from fractions import Fraction
-
 from python.src.common import Day
 
 
@@ -8,45 +6,36 @@ class Dec10(Day):
 
     def __init__(self, filename=None, instructions=None):
         super().__init__(2019, 10, instructions, filename)
+        self.max_x, self.max_y, self.asteroids = self.instructions
         self.best_position = (-1, -1)
 
     @staticmethod
     def parse_instructions(instructions):
         pos = list()
         for y, row in enumerate(instructions):
-            for x, c in enumerate(row):
+            for x, c in enumerate(row.strip()):
                 if c != '.':
                     pos.append((x, y))
-        return pos
+        return len(instructions[0]), len(instructions), pos
 
     def part_1(self):
         best = 0
-        satellites = sorted(self.instructions)
-        shit = dict()
-
-        for i, me in enumerate(satellites):
+        for me in self.asteroids:
             lines_of_sight = set()
-            l = defaultdict(list)
-            m = dict()
-            for j, other in enumerate(satellites):
-                if i == j:
+            for other in self.asteroids:
+                if me == other:
                     continue
-                if me[1] == other[1]:
-                    #slope = '<' if me[0] < other[0] else '>'
-                    slope = -1000 if me[0] < other[0] else 1000
-                elif me[0] == other[0]:
-                    # slope = 'v' if me[1] < other[1] else '^'
-                    slope = -2000 if me[1] < other[1] else 2000
-                else:
-                    slope = Fraction(me[1] - other[1], (me[0] - other[0]))
-                lines_of_sight.add(slope)
-                l[slope].append(other)
-                m[other] = slope
+                sign = me < other
+
+                try:
+                    angle = (me[1]-other[1])/(me[0]-other[0])
+                except ZeroDivisionError:
+                    angle = 1000
+                lines_of_sight.add((sign, angle))
+
             if len(lines_of_sight) > best:
                 best = len(lines_of_sight)
                 self.best_position = me
-            shit[me] = l, m
-
         return best
 
     def part_2(self):
