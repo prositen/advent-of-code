a453abae0d648b8119e09bc7e1b32d95e8017e25
Anna Holmgren <prositen@prositen.com>
2020-12-01 06:34:42 +0100

    2020-12-01 + test fixes for 2019-12-19

--- python/test/y2019/test_dec17.py
+++ python/test/y2019/test_dec17.py
@@ -15,7 +15,23 @@ class TestDec17(unittest.TestCase):
                 "..#####...^.."]
 
         d = Dec17()
+        d.height = len(view)
+        d.width = len(view[0])
+
         self.assertEqual(76, d.alignment_parameters(view))
+
+    def test_part_2(self):
+        d = Dec17()
+        path = ['R', '8', 'R', '8', 'R', '4', 'R', '4', 'R', '8', 'L', '6', 'L', '2',
+                'R', '4', 'R', '4', 'R', '8', 'R', '8', 'R', '8', 'L', '6', 'L', '2']
+        sub = dict()
+        main, sub['A'], sub['B'], sub['C'] = d.compress_path(path)
+        result = ''
+        for prog in main.split(','):
+            result += sub[prog] + ','
+
+        result = result[:-1]
+        self.assertEqual(','.join(path), result)
 
 
 if __name__ == '__main__':
