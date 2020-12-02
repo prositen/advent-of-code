889022756165b960d2618b0d9d7b468d66de4ca3
Anna Holmgren <anna.holmgren@feelgood.se>
2020-12-02 06:18:28 +0100

    2020-12-02

---
+++ python/src/y2020/dec02.py
@@ -0,0 +1,43 @@
+import itertools
+
+from python.src.common import Day, timer
+
+
+class Dec02(Day):
+
+    def __init__(self, instructions=None, filename=None):
+        super().__init__(2020, 2, instructions, filename)
+
+    @staticmethod
+    def parse_instructions(instructions):
+        rules = list()
+        for i in instructions:
+            rng, letter, pwd = i.split(' ')
+            n1, n2 = (int(n) for n in rng.split('-'))
+            rules.append(((n1, n2, letter[0]), pwd.strip()))
+        return rules
+
+    @staticmethod
+    def is_valid_first(rule, pwd):
+        _from, _to, _letter = rule
+        c = pwd.count(_letter)
+        return _from <= c <= _to
+
+    @staticmethod
+    def is_valid_second(rule, pwd):
+        _i1, _i2, _letter = rule
+        return (pwd[_i1 - 1] == _letter) ^ (pwd[_i2 - 1] == _letter)
+
+    @timer(part=1)
+    def part_1(self):
+        return sum(self.is_valid_first(rule, pwd) for rule, pwd in self.instructions)
+
+    @timer(part=2)
+    def part_2(self):
+        return sum(self.is_valid_second(rule, pwd) for rule, pwd in self.instructions)
+
+
+if __name__ == '__main__':
+    d = Dec02()
+    d.part_1()
+    d.part_2()
