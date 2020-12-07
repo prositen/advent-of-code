452cfb8b8fbbd482731af0fbcaa6a58d7eb149a3
Anna Holmgren <prositen@prositen.com>
2020-12-07 22:28:26 +0100

    2019-12-21

---
+++ python/src/y2019/dec21.py
@@ -0,0 +1,73 @@
+from python.src.common import Day, timer, Timer
+from src.y2019.intcode import IntCode
+
+
+class Dec21(Day):
+
+    def __init__(self, instructions=None, filename=None):
+        super().__init__(2019, 21, instructions, filename)
+
+    @staticmethod
+    def parse_instructions(instructions):
+        return Day.parse_int_line(instructions)
+
+    def program_computer(self, spring_code):
+        ic = IntCode(instructions=self.instructions)
+        for line in spring_code:
+            for c in line:
+                ic.add_input(ord(c))
+            ic.add_input(10)
+        ic.run()
+        if ic.output[-1] < 128:
+            self.display_output(ic)
+            return None
+        else:
+            return ic.get_output()
+
+    @staticmethod
+    def display_output(ic):
+        for c in ic.output:
+            print(chr(c), end='')
+
+    @timer(part=1)
+    def part_1(self):
+        """
+
+        ^(A | B | C) & D
+
+        :return: Hull damage
+        """
+
+        code = [
+            "NOT A J",
+            "NOT B T",
+            "OR J T",  # A or B is a hole
+            "NOT C J",
+            "OR T J",  # A, B or C is a hole
+            "AND D J",  # ..and D is ground
+            "WALK"
+        ]
+        return self.program_computer(spring_code=code)
+
+    @timer(part=2)
+    def part_2(self):
+        code = [
+            "NOT B T",
+            "NOT C J",
+            "OR T J",   # B or C is a hole
+            "AND D J",  # ..and D is ground
+            "AND H J",  # and H is ground
+
+            "NOT A T",  # A is a hole, panic jump!
+            "OR T J",
+
+            "RUN"
+        ]
+        return self.program_computer(spring_code=code)
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        d = Dec21()
+        d.part_1()
+        d.part_2()
