e51b4dbc35d8a841eb3228d99accf2166d625e7d
Anna Holmgren <prositen@prositen.com>
2020-12-10 10:45:05 +0100

    2020-12-10 part 2

--- python/src/y2020/dec10.py
+++ python/src/y2020/dec10.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from python.src.common import Day, Timer, timer
 
 
@@ -5,7 +7,7 @@ class Dec10(Day):
 
     def __init__(self, instructions=None, filename=None):
         super().__init__(2020, 10, instructions, filename)
-        self.adapters = [0] + self.instructions + [self.instructions[-1]+3]
+        self.adapters = [0] + self.instructions + [self.instructions[-1] + 3]
 
     @staticmethod
     def parse_instructions(instructions):
@@ -15,16 +17,26 @@ class Dec10(Day):
 
     @timer(part=1)
     def part_1(self):
-        diffs = [self.adapters[c] - self.adapters[c-1] for c in range(1, len(self.adapters))]
-        return diffs.count(1)  * diffs.count(3)
+        diffs = [self.adapters[c] - self.adapters[c - 1] for c in range(1, len(self.adapters))]
+        return diffs.count(1) * diffs.count(3)
 
     @timer(part=2)
     def part_2(self):
-        return 2
+        """ Calculate how many possible ways you can reach the adapter
+        with joltage X by summing up the ways to reach adapters with
+        joltage X-1,2 and 3.
+        :return: Number of adapter arrangements
+        """
+        ways = defaultdict(int)
+        for adapter in self.adapters:
+            if adapter == 0:
+                c = 1
+            else:
+                c = sum(ways[adapter - x] for x in (1, 2, 3))
+            ways[adapter] = c
+        return ways[self.adapters[-1]]
 
 
 if __name__ == '__main__':
     with Timer():
-        d = Dec10()
-        print("Part 1: ", d.part_1())
-        print("Part 2: ", d.part_2())
+        Dec10().run_day()
