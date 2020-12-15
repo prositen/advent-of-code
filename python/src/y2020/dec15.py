c6f39f21440629d83be0c990dcd5d41c2969b2ed
Anna Holmgren <prositen@prositen.com>
2020-12-15 16:38:10 +0100

    Tweak for 2020-12-15

---
+++ python/src/y2020/dec15.py
@@ -0,0 +1,42 @@
+from python.src.common import Day, timer, Timer
+
+
+class Dec15(Day):
+
+    def __init__(self, instructions=None, filename=None):
+        super().__init__(2020, 15, instructions, filename)
+
+    @staticmethod
+    def parse_instructions(instructions):
+        return Day.parse_int_line(instructions)
+
+    def play(self, rounds):
+        numbers = dict()
+        turn = 1
+        for n in self.instructions:
+            numbers[n] = turn
+            turn += 1
+        next_number = self.instructions[-1]
+        for turn in range(turn-1, rounds):
+            last_turn = numbers.get(next_number, 0)
+            numbers[next_number] = turn
+            if last_turn:
+                next_number = turn - last_turn
+            else:
+                next_number = 0
+        return next_number
+
+    @timer(part=1)
+    def part_1(self):
+        return self.play(rounds=2020)
+
+    @timer(part=2)
+    def part_2(self):
+        return self.play(rounds=30000000)
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        d = Dec15()
+        d.part_1()
+        d.part_2()
