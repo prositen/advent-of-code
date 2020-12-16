82d2b8675651a4e161e02f3fd8f7ed3023d9d343
Anna Holmgren <prositen@prositen.com>
2020-12-16 14:34:49 +0100

    Parse room info

--- python/src/y2019/dec25.py
+++ python/src/y2019/dec25.py
@@ -11,18 +11,57 @@ class Dec25(Day):
     def parse_instructions(instructions):
         return Day.parse_int_line(instructions)
 
+    @staticmethod
+    def parse_room(output):
+        desc = [row for row in (row.strip() for row in output.splitlines()) if row]
+        room = desc.pop(0)[3:-3]
+        door_start, door_end = 0,0
+        items_start, items_end = 0,0
+        command = 0
+        description = []
+        for index, line in enumerate(desc):
+            if line.startswith('Doors here'):
+                description = desc[0:index-1]
+                door_start = index+1
+            elif line.startswith('Items here'):
+                items_start = index+1
+                if door_start and not door_end:
+                    door_end = index
+            elif line == 'Command?':
+                if not door_end:
+                    door_end = index
+                elif not items_end:
+                    items_end = index
+                command = index
+
+        if not command:
+            raise BaseException('Game over')
+
+        items = []
+        if items_start and items_end:
+            items = [item[2:] for item in desc[items_start:items_end]]
+        doors = []
+        if door_start and door_end:
+            doors = [door[2:] for door in desc[door_start:door_end]]
+        return room, description, doors, items
+
     @timer(part=1)
-    def part_1(self):
+    def play(self):
         ic = IntCode(instructions=self.instructions)
         while True:
-            ic.run_and_wait()
-            print(ic.get_ascii_string())
+            if ic.run_and_wait():
+                break
+            output = ic.get_ascii_string()
+            room, description, doors, items = self.parse_room(output)
+            print(output)
             command = input()
             if command == 'quit':
                 break
             ic.input_ascii_string(command)
 
+
 if __name__ == '__main__':
     with Timer('Total'):
         d = Dec25()
-        d.part_1()
+        d.play()
+        # d.part_1()
