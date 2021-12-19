f4681a21c2451fe7a8e0a4c26b32caae3b61f553
Anna Holmgren <prositen@prositen.com>
2021-12-19 18:12:00 +0100

    2021-12-18

--- python/src/y2021/dec18.py
+++ python/src/y2021/dec18.py
@@ -1,37 +1,11 @@
-import json
+import itertools
 
 from python.src.common import Day, timer, Timer
 
 
-class SnailFishTree(object):
-
-    def __init__(self, left, right, level):
-        self.left = left
-        self.right = right
-        self.level = level
-
-    @staticmethod
-    def from_string(sf_string):
-        return SnailFishTree.from_list(json.loads(sf_string))
-
-    @staticmethod
-    def from_list(sf_list, level=0):
-        left, right = sf_list
-        if isinstance(left, list):
-            left = SnailFishTree.from_list(left, level + 1)
-        if isinstance(right, list):
-            right = SnailFishTree.from_list(right, level + 1)
-        return SnailFishTree(left, right, level)
-
-    def __str__(self):
-        left = str(self.left)
-        right = str(self.right)
-        return f'[{left},{right}]'
-
-
 class SnailFish(object):
     def __init__(self, numbers):
-        self.numbers = numbers
+        self.numbers = [c for c in numbers]
 
     @staticmethod
     def from_string(sf_string):
@@ -49,36 +23,74 @@ class SnailFish(object):
         return SnailFish(numbers)
 
     def explode(self):
-        i = 0
-        for depth, number in self.numbers:
+        for i, (depth, number) in enumerate(self.numbers):
             if depth >= 5:
+                _, right = self.numbers.pop(i + 1)
+                if i > 0:
+                    l_depth, l_num = self.numbers[i - 1]
+                    self.numbers[i - 1] = (l_depth, l_num + number)
+                self.numbers[i] = (depth - 1, 0)
+                if i < len(self.numbers) - 1:
+                    r_depth, r_num = self.numbers[i + 1]
+                    self.numbers[i + 1] = (r_depth, r_num + right)
                 break
-            i += 1
-        else:
-            return self
-        depth, left = self.numbers[i]
-        _, right = self.numbers.pop(i + 1)
-        if i > 1:
-            l_depth, l_num = self.numbers[i - 1]
-            self.numbers[i - 1] = (l_depth, l_num + left)
-        self.numbers[i] = (depth - 1, 0)
-        if i < len(self.numbers) - 1:
-            r_depth, r_num = self.numbers[i + 1]
-            self.numbers[i + 1] = (r_depth, r_num + right)
+
         return self
 
     def split(self):
-        i = 0
-        for depth, number in self.numbers:
+        for i, (depth, number) in enumerate(self.numbers):
             if number >= 10:
+                half, rest = divmod(number, 2)
+                self.numbers[i] = (depth + 1, half)
+                self.numbers.insert(i + 1, (depth + 1, half + rest))
                 break
-        else:
-            return self
+        return self
+
+    def magnitude(self):
+        def mag(items, level):
+            for i, (depth, number) in enumerate(items):
+                if depth == level:
+                    items[i] = (level - 1, 3 * items[i][1] + 2 * items[i + 1][1])
+                    items.pop(i + 1)
+                    return items
+            return items
+
+        l = list(self.numbers)
+        for depth in (4, 3, 2):
+            while len(l) != len(mag(l, depth)):
+                pass
+
+        return 3 * l[0][1] + 2 * l[1][1]
+
     def __eq__(self, other):
         e = [(d1, n1) == (d2, n2)
              for (d1, n1), (d2, n2) in zip(self.numbers,
                                            other.numbers)]
         return all(e)
+
+    def __str__(self):
+        return str(self.numbers)
+
+    def __repr__(self):
+        return f'<SnailFishList {str(self)}>'
+
+    def __add__(self, other):
+        sf = SnailFish([
+            (depth + 1, number)
+            for (depth, number) in self.numbers + other.numbers
+        ])
+
+        changed = True
+        while changed:
+            if str(sf) != str(sf.explode()):
+                continue
+            changed = False
+
+            if str(sf) != str(sf.split()):
+                changed = True
+                continue
+
+        return sf
 
 
 class Dec18(Day):
@@ -89,16 +101,25 @@ class Dec18(Day):
     @staticmethod
     def parse_instructions(instructions):
         return [
-            SnailFishTree.from_string(row) for row in instructions
+            SnailFish.from_string(row) for row in instructions
         ]
+
+    def add_all(self, items):
+        sf = SnailFish(numbers=items[0].numbers)
+        for t2 in items[1:]:
+            sf = sf + t2
+        return sf
 
     @timer(part=1)
     def part_1(self):
-        return 1
+        return self.add_all(self.instructions).magnitude()
 
     @timer(part=2)
     def part_2(self):
-        return 2
+        max_mag = 0
+        for items in itertools.permutations(self.instructions, 2):
+            max_mag = max(max_mag, self.add_all(items).magnitude())
+        return max_mag
 
 
 if __name__ == '__main__':
