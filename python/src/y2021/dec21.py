af6084887284f07341613a8d9b23e60dbd6e6ef1
Anna Holmgren <prositen@prositen.com>
2021-12-21 22:26:34 +0100

    Part 2

--- python/src/y2021/dec21.py
+++ python/src/y2021/dec21.py
@@ -1,3 +1,6 @@
+import itertools
+from collections import Counter, deque, defaultdict
+
 from python.src.common import Day, timer, Timer
 
 
@@ -10,27 +13,50 @@ class Dec21(Day):
     def parse_instructions(instructions):
         return [int(line.split(' ')[-1], 10) for line in instructions]
 
+    @staticmethod
+    def update(pos, score, roll):
+        pos = (pos + roll - 1) % 10 + 1
+        score += pos
+        return pos, score
+
     @timer(part=1)
     def part_1(self):
-        pos = self.instructions
+        pos = [c for c in self.instructions]
         scores = [0, 0]
         dice = 0
         while True:
             for i in (0, 1):
-                roll = (3*dice+6)
+                roll = (3 * dice + 6)
                 dice += 3
-                pos[i] = (pos[i] + roll -1) % 10 + 1
-
-                scores[i] += pos[i]
-
+                pos[i], scores[i] = self.update(pos[i], scores[i], roll)
                 if scores[i] >= 1000:
                     return dice * min(scores)
 
-
-
     @timer(part=2)
     def part_2(self):
-        return 0
+        dice_rolls = Counter(
+            sum(dice) for dice in itertools.product((1, 2, 3), repeat=3)
+        )
+        winner = [0, 0]
+        to_visit = Counter({
+            ((self.instructions[0], self.instructions[1]), (0, 0), 0)
+        })
+        while to_visit:
+            next_visit = Counter()
+            for (pos, score, player), seen in to_visit.items():
+                for roll, amount in dice_rolls.items():
+                    new_pos = list(pos)
+                    new_score = list(score)
+                    new_pos[player], new_score[player] = self.update(pos[player],
+                                                                     score[player],
+                                                                     roll)
+                    if new_score[player] >= 21:
+                        winner[player] += seen * amount
+                    else:
+                        next_visit[(tuple(new_pos),
+                                    tuple(new_score), 1-player)] += (seen * amount)
+            to_visit = next_visit
+        return max(winner)
 
 
 if __name__ == '__main__':
--- python/test/y2021/test_dec21.py
+++ python/test/y2021/test_dec21.py
@@ -4,9 +4,12 @@ from python.src.y2021.dec21 import Dec21
 
 
 class TestDec21(unittest.TestCase):
+    data = ["Player 1 starting position: 4",
+            "Player 2 starting position: 8"]
 
     def test_part_1(self):
-        data = ["Player 1 starting position: 4",
-                "Player 2 starting position: 8"]
 
-        self.assertEqual(739785, Dec21(instructions=data).part_1())
+        self.assertEqual(739785, Dec21(instructions=self.data).part_1())
+
+    def test_part_2(self):
+        self.assertEqual(444356092776315, Dec21(instructions=self.data).part_2())
