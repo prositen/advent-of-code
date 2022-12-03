72f1fe6d2be90893472de34c37c41ff8a391b3bc
Anna Holmgren <prositen@prositen.com>
2022-12-03 14:00:14 +0100

    2022-12-02 Hardcoded ftw (#176)

--- python/src/y2022/dec01.py
+++ python/src/y2022/dec01.py
@@ -13,11 +13,11 @@ class Dec01(Day):
           for g in Day.parse_groups(instructions=instructions)
         ]
 
-    @timer(part=1)
+    @timer(part=1, title='Most calories carried by one elf')
     def part_1(self):
         return max(sum(g) for g in self.instructions)
 
-    @timer(part=2)
+    @timer(part=2, title='Total calories carried by top three elves')
     def part_2(self):
         elves = [sum(g) for g in self.instructions]
         return sum(sorted(elves, reverse=True)[:3])
