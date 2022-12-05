8048e80de2f7ae531eea5785a67bcb2d2e9f2895
Anna Holmgren <anna.holmgren@feelgood.se>
2022-12-05 11:05:58 +0100

    2022-12-05

--- python/src/y2022/dec05.py
+++ python/src/y2022/dec05.py
@@ -20,7 +20,7 @@ class Dec05(Day):
             for index in range(0, len(row), 4):
                 if stack := row[index:index + 4].strip():
                     crate_index = index // 4
-                    if crate_index <= len(stacks):
+                    if crate_index >= len(stacks):
                         stacks.append(list())
                     stacks[crate_index].append(stack[1])
         return stacks, moves
@@ -28,20 +28,17 @@ class Dec05(Day):
     @timer(part=1)
     def part_1(self):
         stacks = [list(s) for s in self.stacks]
-        for move in self.moves:
-            for _ in range(move[0]):
-                if stacks[move[1]]:
-                    stacks[move[2]].append(stacks[move[1]].pop())
+        for (count, _from, _to) in self.moves:
+            for _ in range(count):
+                stacks[_to].append(stacks[_from].pop())
         return ''.join(s[-1] if s else '' for s in stacks)
 
     @timer(part=2)
     def part_2(self):
         stacks = [list(s) for s in self.stacks]
-        for move in self.moves:
-            mover = list()
-            for _ in range(move[0]):
-                mover.append(stacks[move[1]].pop())
-            stacks[move[2]].extend(mover[::-1])
+        for (count, _from, _to) in self.moves:
+            stacks[_to].extend(stacks[_from][-count:])
+            stacks[_from] = stacks[_from][:-count]
         return ''.join(s[-1] if s else '' for s in stacks)
 
 
