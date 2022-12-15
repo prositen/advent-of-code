617f8b4feac1827c39411a6410dfea22e530529f
Anna Holmgren <anna.holmgren@feelgood.se>
2022-12-15 16:40:47 +0100

    2022-12-15 part 1

--- python/src/y2022/dec15.py
+++ python/src/y2022/dec15.py
@@ -4,40 +4,28 @@ from python.src.common import Day, timer, Timer, distance
 
 
 class SensorMap(object):
-    SENSOR = 1
-    BEACON = 2
 
     def __init__(self, sensors):
-        self.sensors = dict()
+        self.sensors = list()
         self.beacons = set()
-        self.min_x = sensors[0][0]
-        self.max_x = sensors[0][0]
         for (my_x, my_y, beacon_x, beacon_y) in sensors:
             dist = distance((my_x, my_y), (beacon_x, beacon_y))
-            self.sensors[(my_x, my_y)] = dist
+            self.sensors.append(((my_x, my_y), dist))
             self.beacons.add((beacon_x, beacon_y))
-            self.min_x = min(self.min_x, my_x - dist)
-            self.max_x = max(self.max_x, my_x + dist)
 
     def scan_row(self, row):
         cells = set()
-        for (sx, sy), dist in self.sensors.items():
+        ranges = list()
+        for (sx, sy), dist in self.sensors:
             distance_to_row = abs(sy - row)
             if distance_to_row <= dist:
-                for dx in range(abs(dist - distance_to_row) + 1):
-                    cells.add(sx + dx)
-                    cells.add(sx - dx)
+                ranges.append(range(sx - abs(dist - distance_to_row),
+                                    sx + abs(dist - distance_to_row) + 1))
+        for r in ranges:
+            for c in r:
+                cells.add(c)
         return cells
 
-    def find_beacon(self, max_x, max_y):
-        cells = set()
-        for row in range(max_x + 1):
-            for (sx, sy), dist in self.sensors.items():
-                distance_to_row = abs(sy - row)
-                if distance_to_row <= dist:
-                    for dx in range(abs(dist - distance_to_row) + 1):
-                        cells.add((sx + dx, y))
-                        cells.add((sx - dx, y))
 
 
 class Dec15(Day, year=2022, day=15):
