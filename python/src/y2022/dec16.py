4c074c0ce91c1e75601be0ba90f464259c182c77
Anna Holmgren <prositen@prositen.com>
2022-12-19 23:06:43 +0100

    2022-12-16 (#190)
    * 2022-12-16 part 1
    
    * 2022-12-16 optimize, hash on current increase instead of open valves
    
    * 2022-12-16 slow as f part 2
    
    * 2022-12-16 optimization
    
    * 2022-12-16 optimization but still shitty
    
    * 2022-12-16 more optimization
    
    * 2022-12-16 Finally something decent wow

---
+++ python/src/y2022/dec16.py
@@ -0,0 +1,79 @@
+import itertools
+import math
+from collections import deque
+from heapq import heappop, heappush
+
+from python.src.common import Day, timer, Timer
+
+
+class Cave(object):
+
+    def __init__(self, valves):
+        self.tunnels = {valve[0]: {v: 1 for v in valve[2]} for valve in valves}
+        self.flow = {valve[0]: valve[1] for valve in valves if valve[1]}
+        self.valves = {v: 1 << i for i, v in enumerate(vv for vv, flow in self.flow.items())}
+
+    def run(self, max_time=30):
+        to_visit = deque()
+        to_visit.append((1, 0, 0, 'AA', 0))
+
+        running_best = dict()
+        best = dict()
+        final_paths = set()
+
+        while to_visit:
+            (time, pressure, open_valves, pos, increase) = to_visit.popleft()
+            state = (open_valves, pos)
+            pressure += increase
+            if time == max_time:
+                best[open_valves] = max(best.get(open_valves, 0), pressure)
+                final_paths.add((open_valves, pressure))
+                continue
+            elif running_best.get(state, -1) >= pressure:
+                continue
+
+            running_best[state] = pressure
+            if (i := self.flow.get(pos, 0)) and not open_valves & self.valves[pos]:
+                new_open = open_valves | self.valves[pos]
+                to_visit.append(
+                    (time + 1, pressure, new_open, pos, increase + i))
+
+            for tunnel, cost in self.tunnels[pos].items():
+                to_visit.append(
+                    (time + cost, pressure, open_valves, tunnel, increase))
+
+        return best
+
+
+class Dec16(Day, year=2022, day=16):
+
+    @staticmethod
+    def parse_instructions(instructions):
+        valves = list()
+        for line in instructions:
+            valve_data, tunnels = line.split(';')
+            valve_name = valve_data[6:8]
+            flow_rate = int(valve_data[23:])
+            tunnels = [t.strip() for t in tunnels[23:].split(', ')]
+            valves.append((valve_name, flow_rate, tunnels))
+        return valves
+
+    @timer(part=1)
+    def part_1(self):
+        pressure = Cave(self.instructions).run().values()
+        return max(pressure)
+
+    @timer(part=2)
+    def part_2(self):
+        pressure = Cave(self.instructions).run(max_time=26)
+        return max(
+            p1 + p2
+            for v1, p1 in pressure.items()
+            for v2, p2 in pressure.items()
+            if not v1 & v2
+        )
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        Dec16().run_day()
---
+++ python/test/y2022/test_dec16.py
@@ -0,0 +1,22 @@
+import unittest
+
+from python.src.y2022.dec16 import Dec16
+
+
+class TestDec16(unittest.TestCase):
+    data = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II""".split('\n')
+
+    def test_part_1(self):
+        self.assertEqual(1651, Dec16(instructions=self.data).part_1())
+
+    def test_part_2(self):
+        self.assertEqual(1707, Dec16(instructions=self.data).part_2())
