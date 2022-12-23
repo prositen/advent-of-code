b5bb0882fd00547c001f82c42655703f24478095
Anna Holmgren <prositen@prositen.com>
2022-12-23 20:02:55 +0100

    2022-12-23

---
+++ python/src/y2022/dec23.py
@@ -0,0 +1,92 @@
+import itertools
+import operator
+from collections import deque, defaultdict
+
+from python.src.common import Day, timer, Timer
+
+
+class Grove(object):
+    def __init__(self, state):
+        self.instructions = deque([
+            # NW, N, NE -> N
+            ([(-1, -1), (-1, 0), (-1, 1)], (-1, 0)),
+            # SW, S, SE -> S
+            ([(1, -1), (1, 0), (1, 1)], (1, 0)),
+            # NW, W, SW -> W
+            ([(-1, -1), (0, -1), (1, -1)], (0, -1)),
+            # NE, E, SE -> E
+            ([(-1, 1), (0, 1), (1, 1)], (0, 1))
+        ])
+        self.delta = list(itertools.product((-1, 0, 1), repeat=2))
+        self.delta.remove((0, 0))
+        self.elves = set(state)
+
+    def add(self, pos, delta):
+        return tuple(map(operator.add, pos, delta))
+
+    def step(self, steps=1):
+        moved = 0
+        while True:
+            new_elves = defaultdict(list)
+            for e in self.elves:
+                if not any(self.add(e, d) in self.elves for d in self.delta):
+                    new_elves[e].append(e)
+                else:
+                    for nb, mv in self.instructions:
+                        if not any((self.add(e, n) in self.elves for n in nb)):
+                            new_pos = self.add(e, mv)
+                            new_elves[new_pos].append(e)
+                            break
+                    else:
+                        new_elves[e].append(e)
+
+            next_elves = set()
+
+            for pos, elves in new_elves.items():
+                if len(elves) > 1:
+                    next_elves.update(elves)
+                else:
+                    next_elves.add(pos)
+
+            moved += 1
+            if  self.elves == next_elves or (steps and moved > steps):
+                return moved
+
+            self.elves = next_elves
+            self.instructions.rotate(-1)
+
+
+    def count(self):
+        x_pos = sorted(e[1] for e in self.elves)
+        y_pos = sorted(e[0] for e in self.elves)
+        min_x, max_x = x_pos[0], x_pos[-1]
+        min_y, max_y = y_pos[0], y_pos[-1]
+        total = (1 + max_x - min_x) * (1 + max_y - min_y)
+        return total - len(self.elves)
+
+
+class Dec23(Day, year=2022, day=23):
+
+    @staticmethod
+    def parse_instructions(instructions):
+        elves = list()
+        for y, line in enumerate(instructions):
+            for x, char in enumerate(line):
+                if char == '#': elves.append((y,x))
+        return elves
+
+    @timer(part=1)
+    def part_1(self):
+        g = Grove(state=self.instructions)
+        g.step(10)
+        return g.count()
+
+    @timer(part=2)
+    def part_2(self):
+        return Grove(state=self.instructions).step(None)
+
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        Dec23().run_day()
