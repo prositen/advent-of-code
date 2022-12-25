ff1432b7178e614d636e49d9a06d438954dbf0aa
Anna Holmgren <prositen@prositen.com>
2022-12-25 20:00:25 +0100

    2022-12-17 Part 2

--- python/src/y2022/dec17.py
+++ python/src/y2022/dec17.py
@@ -24,72 +24,98 @@ class Chamber(object):
         self.max_height = 0
         self.width = 7
         self.block_rest_count = 0
-        self.shapes = deque([
+        self.shapes = [
             Block.from_lists(['####']),
             Block.from_lists(['.#.', '###', '.#.']),
             Block.from_lists(['..#', '..#', '###']),
             Block.from_lists(['#', '#', '#', '#']),
             Block.from_lists(['##', '##'])
-        ])
+        ]
+        self.shape_index = 0
         self.resting_blocks = set()
         self.resting_blocks.update({
             (0, n) for n in range(self.width)
         })
         self.block = None
-        self.jets = deque(jets)
+        self.jets = jets
+        self.jet_index = 0
+        self.cycle_found = False
 
     def push(self):
-        delta = 1 if self.jets[0] == '>' else -1
-        #pp = 'right' if delta==1 else 'left'
-        self.jets.rotate(-1)
+        delta = 1 if self.jets[self.jet_index] == '>' else -1
+        # pp = 'right' if delta==1 else 'left'
+        self.jet_index = (self.jet_index + 1) % len(self.jets)
         blocks = {(b[0], b[1] + delta) for b in self.block.shape}
         if any(x in (self.width, -1) for _, x in blocks):
-            #print(f'Jet of gas pushes rock {pp}, but nothing happens')
+            # print(f'Jet of gas pushes rock {pp}, but nothing happens')
             return
         if blocks.intersection(self.resting_blocks):
-            #print(f'Jet of gas pushes rock {pp}, but nothing happens')
+            # print(f'Jet of gas pushes rock {pp}, but nothing happens')
             return
-        #print(f'Jet of gas pushes rock {pp}')
+        # print(f'Jet of gas pushes rock {pp}')
         self.block.shape = blocks
-        #self.print()
+        # self.print()
 
     def fall(self):
         blocks_below = {(b[0] - 1, b[1]) for b in self.block.shape}
         if blocks_below.intersection(self.resting_blocks):
             # We hit something!
             self.resting_blocks.update(self.block.shape)
-            self.max_height = max(self.max_height, max(y for y,_ in self.block.shape))
+            self.max_height = max(self.max_height, max(y for y, _ in self.block.shape))
 
             self.block = None
-            #print('Rock falls 1 unit, causing it to come to rest.')
+            # print('Rock falls 1 unit, causing it to come to rest.')
             self.block_rest_count += 1
         else:
-            #print('Rock falls 1 unit')
+            # print('Rock falls 1 unit')
             self.block.shape = blocks_below
 
     def print(self):
         print()
         for y in range(self.max_height, 0, -1):
             line = ''.join("#" if (y, x) in self.resting_blocks
-                            else "@" if self.block and (y, x) in self.block.shape else "." for x in
-                            range(0, self.width))
+                           else "@" if self.block and (y, x) in self.block.shape else "." for x in
+                           range(0, self.width))
             print(f'|{line}|')
         print('+', '-' * (self.width - 1), '+')
 
     def step(self):
-        if not self.block:
-            #print('Rock begins falling')
-            self.block = Block(shape=self.shapes[0].shape,
-                               pos=(self.max_height+4, 2))
-            self.shapes.rotate(-1)
         self.push()
         self.fall()
 
+    def footprint(self):
+        max_y = [0, 0, 0, 0, 0, 0, 0]
+        for y, x in self.resting_blocks:
+            max_y[x] = max(max_y[x], y)
+        min_y = min(max_y)
+        max_y = (y - min_y for y in max_y)
+        return (self.shape_index, self.jet_index,
+                *max_y)
+
     def run(self, resting_rocks):
-        #print()
+        visited = dict()
+        heights = list()
         while self.block_rest_count < resting_rocks:
-            self.step()
-       #  self.print()
+            heights.append(self.max_height)
+            state = self.footprint()
+            if state in visited:
+                stored_block_count = visited[state]
+                cycle_length = self.block_rest_count - stored_block_count
+                diff_heights = self.max_height - heights[stored_block_count]
+                cycle_count, remaining_blocks = divmod(resting_rocks-stored_block_count,
+                                                       cycle_length)
+                height = heights[remaining_blocks + stored_block_count]
+                return height + cycle_count * diff_heights
+            else:
+                visited[state] = self.block_rest_count
+            # print('Rock begins falling')
+            self.block = Block(shape=self.shapes[self.shape_index].shape,
+                               pos=(self.max_height + 4, 2))
+            self.shape_index = (self.shape_index + 1) % len(self.shapes)
+            while self.block:
+                self.step()
+
+        #  self.print()
         return self.max_height
 
 
@@ -98,6 +124,7 @@ class Dec17(Day, year=2022, day=17):
     @staticmethod
     def parse_instructions(instructions):
         return instructions[0]
+
     @timer(part=1)
     def part_1(self):
         return Chamber(jets=self.instructions).run(2022)
--- python/test/y2022/test_dec17.py
+++ python/test/y2022/test_dec17.py
@@ -9,6 +9,6 @@ class TestDec17(unittest.TestCase):
     def test_part_1(self):
         self.assertEqual(3068, Dec17(instructions=self.data).part_1())
 
-    def atest_part_2(self):
+    def test_part_2(self):
         self.assertEqual(1514285714288,
                          Dec17(instructions=self.data).part_2())
