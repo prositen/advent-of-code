62fb300e95592f96e5d20dd49b22df7df54f448a
Anna Holmgren <prositen@prositen.com>
2023-12-01 08:21:26 +0100

    2023-12-01

---
+++ python/src/y2023/dec01.py
@@ -0,0 +1,52 @@
+from python.src.common import Day, timer, Timer
+
+
+class Dec01(Day, year=2023, day=1):
+
+    @staticmethod
+    def first_digit(line):
+        for c in line:
+            if '0' <= c <= '9':
+                return int(c)
+
+    @timer(part=1)
+    def part_1(self):
+        return sum(10 * self.first_digit(line) + self.first_digit(line[::-1])
+                   for line in self.instructions)
+
+    digits = {
+        'z': {'zero': 0},
+        'o': {'one': 1},
+        't': {'two': 2, 'three': 3},
+        'f': {'four': 4, 'five': 5},
+        's': {'six': 6, 'seven': 7},
+        'e': {'eight': 8},
+        'n': {'nine': 9}
+    }
+
+    @classmethod
+    def tokenize(cls, line):
+        tokens = list()
+        for i in range(len(line)):
+            c = line[i]
+            if '0' <= c <= '9':
+                tokens.append(int(c))
+            elif c in cls.digits:
+                for num, dig in cls.digits[c].items():
+                    if line[i:].startswith(num):
+                        tokens.append(dig)
+                        break
+            i += 1
+
+    @timer(part=2)
+    def part_2(self):
+        calibration_value = 0
+        for line in self.instructions:
+            tokens = self.tokenize(line)
+            calibration_value += (10 * tokens[0] + tokens[-1])
+        return calibration_value
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        Dec01().run_day()
---
+++ python/test/y2023/test_dec01.py
@@ -0,0 +1,30 @@
+import unittest
+
+from python.src.y2023.dec01 import Dec01
+
+
+class TestDec01(unittest.TestCase):
+
+    def test_part_1_calibration_values(self):
+        data = [
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
+        ]
+        self.assertEqual(142, Dec01(instructions=data).part_1())
+
+    def test_part_2(self):
+        data = [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"
+        ]
+        self.assertEqual(281, Dec01(instructions=data).part_2())
+
+if __name__ == '__main__':
+    unittest.main()
