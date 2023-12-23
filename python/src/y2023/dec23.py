9a1df4e9760b5557b66bbc6a98b4d9714d65fce8
Anna Holmgren <anna.holmgren@feelgood.se>
2023-12-23 22:11:18 +0100

    2023-12-23 Broken shit

--- python/src/y2023/dec23.py
+++ python/src/y2023/dec23.py
@@ -1,4 +1,5 @@
-from collections import deque
+from collections import deque, defaultdict
+from heapq import heappop, heappush
 
 from python.src.common import Day, timer, Timer
 
@@ -17,21 +18,19 @@ class HikingTrails(object):
             '<': (0, -1)
         }
 
-        self.target = len(trail_map)-1, len(trail_map[0]) - 2
+        self.target = len(trail_map) - 1, len(trail_map[0]) - 2
 
-    def move(self,ignore_slopes=False):
+    def move(self, ignore_slopes=False):
         to_visit = deque()
         if ignore_slopes:
-            to_visit.append((self.target, None))
-            self.target = (0,1)
+            to_visit.append((self.target, set()))
+            self.target = (0, 1)
         else:
-            to_visit.append(((0, 1), None))
+            to_visit.append(((0, 1), set()))
 
         longest = 0
         while to_visit:
             (y, x), visited = to_visit.popleft()
-            if visited is None:
-                visited = set()
             if (y, x) == self.target:
                 longest = max(longest, len(visited))
                 continue
@@ -45,22 +44,94 @@ class HikingTrails(object):
             else:
                 for dy, dx in self.delta.values():
                     ny, nx = y + dy, x + dx
-                    if 0<= ny < len(self.map) and (ny, nx) not in visited and self.map[ny][nx] != '#':
+                    if 0 <= ny < len(self.map) and (ny, nx) not in visited and self.map[ny][
+                        nx] != '#':
                         to_visit.append(((ny, nx), set(visited)))
         return longest
+
+    def find_straight_path(self, pos, came_from, with_slopes=False):
+        to_visit = deque()
+        to_visit.append((pos, set(came_from), 0))
+        y, x = pos
+        while to_visit:
+            (y, x), visited, length = to_visit.popleft()
+            length += 1
+            if (y, x) == self.target:
+                return (y, x), length, set()
+
+            valid_nbs = set()
+            if with_slopes and self.map[y][x] in self.delta:
+                dy, dx = self.delta[(self.map[y][x])]
+                ny, nx = dy + y, dx + x
+                if (ny, nx) not in visited:
+                    valid_nbs = {(ny, nx)}
+            else:
+                for dy, dx in self.delta.values():
+                    ny, nx = y + dy, x + dx
+                    if (0 <= ny < len(self.map)
+                            and (ny, nx) not in visited
+                            and self.map[ny][nx] != '#'):
+                        valid_nbs.add((ny, nx))
+            visited.add((y, x))
+
+            if len(valid_nbs) > 1:
+                return (y, x), length, valid_nbs
+            else:
+                to_visit.extend(
+                    (nb, set(visited), length) for nb in valid_nbs if nb not in visited)
+        return (y, x), 0, set()
+
+    def make_graph(self, with_slopes=False):
+        nodes = dict()
+
+        to_visit = deque()
+        to_visit.append(((0, 1), set()))
+
+        while to_visit:
+            start_pos, came_from = to_visit.popleft()
+            if start_pos not in nodes:
+                end_pos, length, nbs = self.find_straight_path(pos=start_pos,
+                                                               came_from=came_from,
+                                                               with_slopes=with_slopes)
+                nodes[start_pos] = {
+                    'length': length,
+                    'end': end_pos,
+                    'nbs': nbs
+                }
+
+                to_visit.extend(((nb, {end_pos}) for nb in nbs))
+
+        to_visit = [(0, (0, 1), tuple())]
+        longest = 0
+
+        while to_visit:
+            length, start_pos, visited = heappop(to_visit)
+            node = nodes[start_pos]
+            length -= node['length']
+            if node['end'] == self.target:
+                longest = min(length, longest)
+                continue
+
+            elif node['end'] in visited:
+                continue
+
+            new_visited = visited + tuple(node['end'], )
+            for nb in node['nbs']:
+                if nb not in visited:
+                    heappush(to_visit, (length, nb, new_visited))
+
+        return -longest - 1
 
 
 class Dec23(Day, year=2023, day=23):
 
     @timer(part=1, title='Longest hike')
     def part_1(self):
-        ht = HikingTrails(self.instructions)
-        return ht.move()
+        return HikingTrails(self.instructions).make_graph(with_slopes=True)
 
     @timer(part=2)
     def part_2(self):
-        ht = HikingTrails(self.instructions)
-        return ht.move(ignore_slopes=True)
+        return HikingTrails(self.instructions).make_graph(with_slopes=False)
 
 
 if __name__ == '__main__':
