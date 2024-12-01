08d7f61691b63b8d7982746ed8251eeb57df6a0b
Anna Holmgren <anna.holmgren@feelgood.se>
2024-12-01 12:29:15 +0100

    2024-12-01

--- python/src/y2024/dec01.py
+++ python/src/y2024/dec01.py
@@ -8,19 +8,19 @@ class Dec01(Day, year=2024, day=1, title='Historian Hysteria'):
     @classmethod
     def parse_instructions(cls, instructions):
         lines = cls.parse_multiple_ints_per_line(instructions, separator=r'\s+')
-        return [l[0] for l in lines], [l[1] for l in lines]
+        return [line[0] for line in lines], [line[1] for line in lines]
 
     @timer(part=1)
     def part_1(self):
         sorted_left = sorted(self.instructions[0])
         sorted_right = sorted(self.instructions[1])
-        return sum(abs(x-y) for x, y in zip(sorted_left, sorted_right))
+        return sum(abs(x - y) for x, y in zip(sorted_left, sorted_right))
 
     @timer(part=2)
     def part_2(self):
         left = self.instructions[0]
         right = Counter(self.instructions[1])
-        return sum(l * right[l] for l in left)
+        return sum(item * right[item] for item in left)
 
 
 if __name__ == '__main__':
