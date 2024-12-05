a65e79e132a8d2f83730a485764a2e0df1d4d4a1
Anna Holmgren <anna.holmgren@feelgood.se>
2024-12-05 13:55:07 +0100

    2024-12-05

--- python/src/y2024/dec05.py
+++ python/src/y2024/dec05.py
@@ -5,6 +5,12 @@ from python.src.common import Day, timer, Timer
 
 class Dec05(Day, year=2024, day=5, title='Print queue'):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rules = self.instructions[0]
+        self.pages = self.instructions[1]
+        self.cache = dict()
+
     @staticmethod
     def parse_instructions(instructions):
         groups = Day.parse_groups(instructions)
@@ -16,19 +22,22 @@ class Dec05(Day, year=2024, day=5, title='Print queue'):
         return rules, pages
 
     def page_order_is_valid(self, pages):
-        return not any([
-            set(pages[i + 1:]).intersection(self.instructions[0][page])
-            for i, page in enumerate(pages)
-        ])
+        index = tuple(pages)
+        if index not in self.cache:
+            self.cache[index] = not any([
+                set(pages[i + 1:]) & self.rules[page]
+                for i, page in enumerate(pages)
+            ])
+        return self.cache[index]
 
     def reorder_pages(self, pages):
         # Sort pages by number of prerequisites
         page_requirements = sorted(
             {
-                page: set(self.instructions[0][page]).intersection(pages)
+                page: len(set(pages) & self.rules[page])
                 for page in pages
             }.items(),
-            key=lambda c: len(c[1])
+            key=lambda c: c[1]
         )
         return list(p[0] for p in page_requirements)
 
@@ -37,13 +46,13 @@ class Dec05(Day, year=2024, day=5, title='Print queue'):
         return sum(
             pages[len(pages) // 2]
             if self.page_order_is_valid(pages) else 0
-            for pages in self.instructions[1]
+            for pages in self.pages
         )
 
     @timer(part=2)
     def part_2(self):
         page_sum = 0
-        for pages in self.instructions[1]:
+        for pages in self.pages:
             if not self.page_order_is_valid(pages):
                 pages = self.reorder_pages(pages)
                 page_sum += pages[len(pages) // 2]
