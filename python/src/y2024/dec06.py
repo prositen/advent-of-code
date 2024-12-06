96b1ad6791ac6ce26cdc6e15f6ab668919391ec1
Anna Holmgren <anna.holmgren@feelgood.se>
2024-12-06 16:44:49 +0100

    2024-12-05 Rewrite doesn't work :'(

--- python/src/y2024/dec06.py
+++ python/src/y2024/dec06.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
-from enum import auto, IntEnum
+from enum import StrEnum
 from typing import List
 
 from python.src.common import Day, timer, Timer
 
 
-class Facing(IntEnum):
-    North = auto()
-    East = auto()
-    South = auto()
-    West = auto()
+class Facing(StrEnum):
+    North = 'north'
+    East = 'east'
+    South = 'south'
+    West = 'west'
 
     def turn_right(self):
         match self.value:
@@ -39,54 +39,99 @@ class GuardPatrol(object):
         self.map = [[c for c in row] for row in lab_map]
         self.visited = defaultdict(set)
         self.facing = Facing.North
-        self.possible_obstacles = set()
+        max_y = len(self.map)
+        max_x = len(self.map[0])
+        self.obstacles_by_row = defaultdict(lambda: {-1, max_y + 1})
+        self.obstacles_by_col = defaultdict(lambda: {-1, max_x + 1})
         for y, row in enumerate(lab_map):
-            if (x := row.find('^')) > -1:
-                self.pos = (y, x)
-                self.start_pos = (y, x)
-                self.map[y][x] = '.'
-                self.visited[self.pos].add(self.facing)
-                break
+            for x, col in enumerate(row):
+                if col == '^':
+                    self.pos = (y, x)
+                    self.start_pos = (y, x)
+                    self.map[y][x] = '.'
+                    self.visited[self.pos].add(self.facing)
+                elif col == '#':
+                    self.obstacles_by_row[y].add(x)
+                    self.obstacles_by_col[x].add(y)
 
     def patrol(self, add_obstacles=False, detect_loops=False):
         next_pos = self.pos
+        obstacles = 0
         while 0 <= next_pos[0] < len(self.map) and 0 <= next_pos[1] < len(self.map[0]):
             if '.' != self.map[next_pos[0]][next_pos[1]]:
                 self.facing = self.facing.turn_right()
             else:
                 if detect_loops and self.facing in self.visited.get(next_pos, set()):
-                    return True
+                    return next_pos != self.start_pos
                 self.pos = next_pos
                 if add_obstacles and self.scan_ahead():
-                    self.possible_obstacles.add(self.facing.position_ahead(next_pos))
+                    obstacles += 1
                 self.visited[next_pos].add(self.facing)
             next_pos = self.facing.position_ahead(self.pos)
 
         if add_obstacles:
-            self.possible_obstacles.discard(self.start_pos)
-            return len(self.possible_obstacles)
+            return obstacles
         elif detect_loops:
             return False
         else:
             return len(self.visited)
 
-    def scan_ahead(self):
-        possible_turn = self.facing.turn_right()
-        next_pos = possible_turn.position_ahead(self.pos)
+    def patrol2(self):
+        next_pos = self.pos
         while 0 <= next_pos[0] < len(self.map) and 0 <= next_pos[1] < len(self.map[0]):
             if '.' != self.map[next_pos[0]][next_pos[1]]:
-                return False
-            if possible_turn in self.visited.get(next_pos, set()):
-                return True
-            next_pos = possible_turn.position_ahead(next_pos)
+                self.facing = self.facing.turn_right()
+            else:
+                if self.facing in self.visited.get(next_pos, set()):
+                    return True
+                self.pos = next_pos
+                self.visited[next_pos].add(self.facing)
+            next_pos = self.facing.position_ahead(self.pos)
+        return False
+
+    def is_loop(self):
+        next_pos = self.pos
+        while 0 <= next_pos[0] < len(self.map) and 0 <= next_pos[1] < len(self.map[0]):
+            next_pos = self.teleport(next_pos, self.facing)
+            if self.facing in self.visited.get(next_pos, set()):
+                return next_pos != self.start_pos
+            self.visited[next_pos].add(self.facing)
+            self.facing = self.facing.turn_right()
+
+    def teleport(self, pos, facing):
+        obs_y, obs_x = pos
+        match facing:
+            case Facing.North:
+                obs_y = max(y for y in self.obstacles_by_col[pos[1]]
+                            if y < pos[0]) + 1
+            case Facing.East:
+                obs_x = min(x for x in self.obstacles_by_row[pos[0]]
+                            if x > pos[1]) - 1
+            case Facing.South:
+                obs_y = min(y for y in self.obstacles_by_col[pos[1]]
+                            if y > pos[0]) - 1
+            case Facing.West:
+                obs_x = max(x for x in self.obstacles_by_row[pos[0]]
+                            if x < pos[1]) + 1
+        return obs_y, obs_x
+
+    def scan_ahead(self):
+        next_facing = self.facing.turn_right()
+        next_pos = self.teleport(self.pos, next_facing)
+        return next_facing in self.visited.get(next_pos, set())
 
     def reset_and_set_pos(self, add_pos, remove_pos=None):
         self.visited = defaultdict(set)
         self.facing = Facing.North
         self.pos = self.start_pos
-        self.map[add_pos[0]][add_pos[1]] = '#'
         if remove_pos:
+            self.obstacles_by_row[remove_pos[0]].discard(remove_pos[1])
+            self.obstacles_by_col[remove_pos[1]].discard(remove_pos[0])
             self.map[remove_pos[0]][remove_pos[1]] = '.'
+
+        self.obstacles_by_row[add_pos[0]].add(add_pos[1])
+        self.obstacles_by_col[add_pos[1]].add(add_pos[0])
+        self.map[add_pos[0]][add_pos[1]] = '#'
 
 
 class Dec06(Day, year=2024, day=6, title='Guard Gallivant'):
@@ -97,15 +142,23 @@ class Dec06(Day, year=2024, day=6, title='Guard Gallivant'):
         return gp.patrol()
 
     @timer(part=2)
+    def part_2_breezy(self):
+        gp = GuardPatrol(self.instructions)
+        return gp.patrol(add_obstacles=True)
+
     def part_2(self):
+        return self.part_2_brute()
+
+    @timer(part=2)
+    def part_2_brute(self):
         gp = GuardPatrol(self.instructions)
         gp.patrol()
         remove_pos = None
         num_obstacles = 0
-        gp.visited.pop(gp.start_pos, None)
+        gp.visited.pop(gp.start_pos)
         for pos in list(gp.visited):
             gp.reset_and_set_pos(add_pos=pos, remove_pos=remove_pos)
-            if gp.patrol(detect_loops=True):
+            if gp.patrol2():  # gp.is_loop():
                 num_obstacles += 1
             remove_pos = pos
         return num_obstacles
