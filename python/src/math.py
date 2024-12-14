811cc929152411e3f17544acd66fd2c40654c8ae
Anna Holmgren <prositen@prositen.com>
2024-12-14 14:15:21 +0100

    2024-12-13 (#233)
    * 2024-12-13
    
    * Remove unused code

--- python/src/math.py
+++ python/src/math.py
@@ -38,3 +38,22 @@ def proper_divs(n):
     divs = {reduce(MUL, (pf ** m for pf, m in zip(factors, multis)), 1)
             for multis in multiplicities}
     return divs or ({1} if n != 1 else set())
+
+
+def line_intersection(line1, line2):
+    # Stolen from stack overflow
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return None
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+
+    return round(x), round(y)
---
+++ python/src/y2024/dec13.py
@@ -0,0 +1,69 @@
+import re
+
+from python.src.common import Day, timer, Timer
+
+
+class ClawMachine(object):
+
+    def __init__(self, instructions):
+        self.ax, self.ay = instructions[0]
+        self.bx, self.by = instructions[1]
+        self.tx, self.ty = instructions[2]
+
+    def play(self, delta=0):
+        """
+            Button A: (ax, ay)
+            Button B: (bx, by)
+            Target: (tx, ty)
+
+            M * ax + N * bx = tx
+            M * ay + N * by = ty
+
+            M = (tx * by - bx*ty) / (ax * by - bx*ay)
+            N = (tx - M*ax) / bx
+
+        """
+        self.tx += delta
+        self.ty += delta
+        a_presses, m = divmod(
+            self.tx * self.by - self.ty * self.bx,
+            self.ax * self.by - self.ay * self.bx)
+        if m:
+            return 0
+        b_presses, m = divmod(self.tx - a_presses * self.ax,
+                              self.bx)
+        if m:
+            return 0
+
+        return 3 * a_presses + b_presses
+
+
+class Dec13(Day, year=2024, day=13):
+
+    @staticmethod
+    def parse_instructions(instructions):
+        pattern: re.Pattern = re.compile(r'(\d+)')
+        return [
+            tuple(tuple(map(int, pattern.findall(group[n])))
+                  for n in range(len(group)))
+            for group in Day.parse_groups(instructions)
+        ]
+
+    @timer(part=1)
+    def part_1(self):
+        return sum(
+            ClawMachine(data).play()
+            for data in self.instructions
+        )
+
+    @timer(part=2)
+    def part_2(self):
+        return sum(
+            ClawMachine(data).play(delta=10_000_000_000_000)
+            for data in self.instructions
+        )
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        Dec13().run_day()
