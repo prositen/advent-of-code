e779c0b62962895ace5c6f961cfb89d72884dcc4
Anna Holmgren <anna.holmgren@feelgood.se>
2024-12-16 15:41:00 +0100

    2024-12-15

--- python/src/y2024/dec15.py
+++ python/src/y2024/dec15.py
@@ -1,59 +1,49 @@
-import sys
-
 from python.src.common import Day, timer, Timer
 
 
 class Warehouse(object):
 
-    def __init__(self, walls, blocks, robot_pos, wide=False):
-        self.walls = set()
-        self.blocks = set()
-        self.wide_block = set()
+    def __init__(self, wh_map, wide=False):
         self.wide = wide
-
-        if wide:
-            for y, x in walls:
-                self.walls.add((y, x * 2))
-                self.walls.add((y, x * 2 + 1))
-            for y, x in blocks:
-                self.blocks.add((y, x * 2))
-                self.wide_block.add((y, x * 2 + 1))
-            self.ry = robot_pos[0]
-            self.rx = 2 * robot_pos[1]
-        else:
-            self.walls = {w for w in walls}
-            self.blocks = {b for b in blocks}
-            self.ry, self.rx = robot_pos
-
-        self.move_blocks = list()
+        if self.wide:
+            wh_map = [
+                line.replace('.', '..')
+                .replace('O', '[]')
+                .replace('#', '##')
+                .replace('@', '@.') for line in wh_map]
+        self.ry, self.rx = divmod(''.join(wh_map).index('@'),
+                                  len(wh_map[0]))
+        self.grid = {
+            (y, x): ch
+            for y, line in enumerate(wh_map)
+            for x, ch in enumerate(line)
+        }
 
     def gps_sum(self):
-        return sum(
-            y * 100 + x
-            for (y, x) in self.blocks
-        )
+        return sum(y * 100 + x
+                   for (y, x) in self.grid
+                   if self.grid[(y, x)] in ('O', '['))
 
-    def push_block(self, by, bx, dy, dx):
-        if (by, bx) in self.walls:
+    def push_block(self, by, bx, dy, dx, dry_run):
+        ny, nx = by + dy, bx + dx
+        if self.grid[ny, nx] == '#':
             return False
-        if (by, bx) not in (self.blocks | self.wide_block):
-            return True
-
-        if not self.push_block(by + dy, bx + dx, dy, dx):
-            return False
-
-        if self.wide and dx == 0:
-            if (by, bx) in self.blocks:
-                if not self.push_block(by + dy, bx + 1, dy, 0):
+        elif self.grid[ny, nx] != '.':
+            if dy == 0 or self.grid[ny, nx] == 'O':
+                if not self.push_block(ny, nx, dy, dx, dry_run):
                     return False
-            elif (by, bx) in self.wide_block:
-                if not self.push_block(by + dy, bx - 1, dy, 0):
+            elif self.grid[ny, nx] == '[':
+                if not (self.push_block(ny, nx, dy, dx, dry_run)
+                        and self.push_block(ny, nx + 1, dy, dx, dry_run)):
+                    return False
+            elif self.grid[ny, nx] == ']':
+                if not (self.push_block(ny, nx, dy, dx, dry_run)
+                        and self.push_block(ny, nx - 1, dy, dx, dry_run)):
                     return False
 
-        if (by, bx) in self.blocks:
-            self.move_blocks.append((by, bx))
-        elif (by, bx) in self.wide_block:
-            self.move_blocks.append((by, bx - 1))
+        if not dry_run:
+            self.grid[by, bx], self.grid[ny, nx] = \
+                self.grid[ny, nx], self.grid[by, bx]
         return True
 
     def move_robot(self, instructions):
@@ -64,19 +54,18 @@ class Warehouse(object):
             '<': (0, -1)
         }
 
-        for i, direction in enumerate(instructions):
+        for direction in instructions:
             dy, dx = _delta[direction]
-            self.move_blocks = list()
-            if self.push_block(self.ry + dy, self.rx + dx, dy, dx):
-                for by, bx in self.move_blocks[::1]:
-                    self.blocks.discard((by, bx))
-                    self.blocks.add((by + dy, bx + dx))
-                    if self.wide:
-                        self.wide_block.discard((by, bx + self.wide))
-                        self.wide_block.add((by + dy, bx + dx + self.wide))
-
+            if self.push_block(self.ry, self.rx, dy, dx, dry_run=True):
+                self.push_block(self.ry, self.rx, dy, dx, dry_run=False)
                 self.ry += dy
                 self.rx += dx
+
+    def __str__(self):
+        max_x = max(self.grid, key=lambda m: m[1])[1] + 1
+        max_y = max(self.grid, key=lambda m: m[0])[0] + 1
+        return '\n'.join(''.join(self.grid[y, x] for x in range(0, max_x))
+                         for y in range(0, max_y))
 
 
 class Dec15(Day, year=2024, day=15):
@@ -84,37 +73,19 @@ class Dec15(Day, year=2024, day=15):
     @staticmethod
     def parse_instructions(instructions):
         groups = Day.parse_groups(instructions)
-        walls = set()
-        blocks = set()
-        start_pos = 0, 0
-        for y, line in enumerate(groups[0]):
-            for x, ch in enumerate(line):
-                match ch:
-                    case '@':
-                        start_pos = y, x
-                    case 'O':
-                        blocks.add((y, x))
-                    case '#':
-                        walls.add((y, x))
-
-        return walls, blocks, start_pos, ''.join(groups[1])
+        return groups[0], ''.join(groups[1])
 
     @timer(part=1)
     def part_1(self):
-        wh = Warehouse(walls=self.instructions[0],
-                       blocks=self.instructions[1],
-                       robot_pos=self.instructions[2])
-        wh.move_robot(self.instructions[3])
+        wh = Warehouse(wh_map=self.instructions[0])
+        wh.move_robot(self.instructions[1])
         return wh.gps_sum()
 
     @timer(part=2)
     def part_2(self):
-        wh = Warehouse(walls=self.instructions[0],
-                       blocks=self.instructions[1],
-                       robot_pos=self.instructions[2],
+        wh = Warehouse(wh_map=self.instructions[0],
                        wide=True)
-        wh.move_robot(self.instructions[3])
-        # wh.print()
+        wh.move_robot(self.instructions[1])
         return wh.gps_sum()
 
 
