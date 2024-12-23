6cc29e6e49bff784240e152a9e8a1978112dc83d
Anna Holmgren <prositen@prositen.com>
2024-12-23 16:38:28 +0100

    2024-12-22 (#242)
    * 2024-12-22 part 1
    
    * 2024-12-22 part 2
    
    * 2024-12-22 part 2

---
+++ python/src/y2024/dec22.py
@@ -0,0 +1,68 @@
+from collections import deque, Counter
+
+from python.src.common import Day, timer, Timer
+
+
+class Secret(object):
+    prune = 16777216
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.changes = deque([0], maxlen=4)
+        self.costs = deque([secret % 10], maxlen=4)
+        self.banana_prices: dict[tuple, int] = dict()
+
+    def next_secret(self):
+        self.secret = ((self.secret << 6) ^ self.secret) % self.prune
+        self.secret = ((self.secret >> 5) ^ self.secret) % self.prune
+        self.secret = ((self.secret << 11) ^ self.secret) % self.prune
+        cost = self.secret % 10
+        self.changes.append(self.costs[-1] - cost)
+        self.costs.append(cost)
+        if len(self.changes) == 4 and (changes := tuple(self.changes)) not in self.banana_prices:
+            self.banana_prices[changes] = cost
+        return self.secret
+
+    def nth_secret(self, n):
+        for _ in range(n):
+            self.next_secret()
+        return self.secret
+
+
+class Dec22(Day, year=2024, day=22):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.secrets = [Secret(seed) for seed in self.instructions]
+        self.has_generated_secrets = False
+
+    def generate_secrets(self):
+        if not self.has_generated_secrets:
+            for secret in self.secrets:
+                secret.nth_secret(2000)
+            self.has_generated_secrets = True
+
+    @staticmethod
+    def parse_instructions(instructions):
+        return Day.parse_int_lines(instructions)
+
+    @timer(part=1)
+    def part_1(self):
+        self.generate_secrets()
+        return sum(
+            secret.secret
+            for secret in self.secrets
+        )
+
+    @timer(part=2)
+    def part_2(self):
+        self.generate_secrets()
+        prices = Counter()
+        for secret in self.secrets:
+            prices.update(secret.banana_prices)
+        return prices.most_common(1)[0][1]
+
+
+if __name__ == '__main__':
+    with Timer('Total'):
+        Dec22().run_day()
---
+++ python/test/y2024/test_dec22.py
@@ -0,0 +1,27 @@
+import unittest
+
+from python.src.y2024.dec22 import Dec22
+from src.y2024.dec22 import Secret
+
+
+class TestDec22(unittest.TestCase):
+    data = ["1", "10", "100", "2024"]
+
+    def test_generate_secrets(self):
+        seed = 123
+        secrets = [15887950, 16495136, 527345, 704524, 1553684,
+                   12683156, 11100544, 12249484, 7753432, 5908254]
+        secret_generator = Secret(seed)
+        for secret in secrets:
+            self.assertEqual(secret, secret_generator.next_secret())
+
+    def test_part_1(self):
+        self.assertEqual(37327623, Dec22(instructions=self.data).part_1())
+
+    def test_part_2(self):
+        data = ["1", "2", "3", "2024"]
+        self.assertEqual(23, Dec22(instructions=data).part_2())
+
+
+if __name__ == '__main__':
+    unittest.main()
