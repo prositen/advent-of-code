8d366699cd268687e354f168b26f10003fcf6f69
Anna Holmgren <prositen@prositen.com>
2024-12-25 17:21:50 +0100

    2024-12-24 Part 2

--- python/src/y2024/dec24.py
+++ python/src/y2024/dec24.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 
 from python.src.common import Day, timer, Timer
 
+
 class FruitMonitor(object):
 
     def __init__(self, register_values):
@@ -11,8 +12,7 @@ class FruitMonitor(object):
         to_run = deque(instructions)
 
         while to_run:
-            instruction  = to_run.popleft()
-            (op1, command, op2, _, target) = instruction
+            (op1, command, op2, _, target) = instruction = to_run.popleft()
             if op1 in self.reg and op2 in self.reg:
                 match command:
                     case 'AND':
@@ -24,10 +24,60 @@ class FruitMonitor(object):
             else:
                 to_run.append(instruction)
 
-
         output_gates = sorted((k for k in self.reg.keys() if k[0] == 'z'), reverse=True)
-        output_bits = ''.join(str(self.reg[o]) for o in output_gates)
-        return int(output_bits, 2)
+        output = 0
+        for o in output_gates:
+            output = output * 2 + self.reg[o]
+        return output
+
+
+class Analyzer(object):
+
+    def __init__(self, gates):
+        self.gates_by_operation = {
+            (g[1], frozenset({g[0], g[2]})): g[4]
+            for g in gates
+        }
+        self.faulty = set()
+
+    def run(self):
+        carry = self.gates_by_operation[('AND', frozenset({'x00', 'y00'}))]
+        for i in range(1, 44):
+            carry = self.adder(i, carry)
+
+    def lookup(self, op, a, b):
+        ab = frozenset({a, b})
+        return self.gates_by_operation.get((op, ab))
+
+    def adder(self, i, carry):
+        x = f'x{i:02}'
+        y = f'y{i:02}'
+        z = f'z{i:02}'
+        xor_1 = self.lookup('XOR', x, y)
+
+        if (and_1 := self.lookup('AND', x, y)) == z:
+            self.faulty.add(z)
+
+        if not (and_2 := self.lookup('AND', xor_1, carry)):
+            self.faulty.add(and_1)
+            self.faulty.add(xor_1)
+            (and_1, xor_1) = (xor_1, and_1)
+            and_2 = self.lookup('AND', xor_1, carry)
+
+        if (xor_2 := self.lookup('XOR', xor_1, carry)) != z:
+            self.faulty.update({xor_2, z})
+            if and_1 == z:
+                xor_2, and_1 = z, xor_2
+
+        if carry := self.lookup('OR', and_1, and_2):
+            if carry == z:
+                self.faulty.add(carry)
+                if xor_2 in self.faulty:
+                    return xor_2
+            return carry
+
+        return self.lookup('OR', and_1, xor_2)
+
 
 class Dec24(Day, year=2024, day=24):
 
@@ -53,7 +103,9 @@ class Dec24(Day, year=2024, day=24):
 
     @timer(part=2)
     def part_2(self):
-        return 0
+        al = Analyzer(self.instructions[1])
+        al.run()
+        return ','.join(sorted(al.faulty))
 
 
 if __name__ == '__main__':
