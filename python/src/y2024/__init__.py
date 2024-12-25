8046621569dd06cf098069775f46de0885d67b37
Anna Holmgren <prositen@prositen.com>
2024-12-25 17:22:11 +0100

    2024-12-24 Part 2

--- python/src/y2024/__init__.py
+++ python/src/y2024/__init__.py
@@ -22,3 +22,4 @@ from python.src.y2024.dec20 import Dec20
 from python.src.y2024.dec21 import Dec21
 from python.src.y2024.dec22 import Dec22
 from python.src.y2024.dec23 import Dec23
+from python.src.y2024.dec24 import Dec24
